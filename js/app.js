31a6cd27b5691ff452d63c43f3a17ad8d5293a20
aidemar <aidemarmag@hotmail.com>
2018-01-19 13:17:34 -0600

    maquetado pagina principal

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  setTimeout(function() {
+     $(".splash-screen").fadeOut(1500);
+ },3000);
+
+ setTimeout(function() {
+      $(".main-section").fadeIn(1500);
+  },3000);
+});
