a098f6ab623ec0e511423576c896ca12cec2943d
aidemar <aidemarmag@hotmail.com>
2018-01-23 06:30:38 -0600

    subiendo funcionalidad boton next

---
+++ README.md
@@ -0,0 +1,7 @@
+Flujo
+
+1. Función que realice el screen-splah.
+2. Boton SING UP que rediriga a una segunda vista, con dropdown con códigos de teléfono por país y un input para ingresar el número telefónico.
+3. Función que habilite el botón de Next cuando se introduzca el número.
+4. Cuando se haga click en Next, se abra un alert donde contenga un código con un número random.
+5. 
--- js/app.js
+++ js/app.js
@@ -11,4 +11,39 @@ $(document).ready(function() {
 });
 
 // Funcionalidad libreria de banderitas
-$("#demo").intlTelInput();
+/*$("#demo").intlTelInput();
+*/
+
+//Funcionalidad view2
+var $phoneInput = $(".phone-input");
+
+
+function loadPage () {
+ $phoneInput.keyup(validatePhone);
+}
+
+//Función para habilitar el botón de Next cuando se escriba el número.
+function validatePhone () {
+	var $containerAddPhone = $("#next");
+
+	if($(this).val().trim().length > 0) {
+		$containerAddPhone.removeAttr("disabled");
+	} else {
+		$containerAddPhone.attr("disabled", true);
+	}
+}
+
+//Función para generar un número random que tendría que aparecer en un alert.
+
+function getRandomArbitrary(min, max) {
+ 	return  Math.floor(Math.random() * (max - min)) + min;
+
+/* 		alert("LAB-" + randomNumber);
+ 	}*/
+ 	 
+
+}
+
+
+
+$(document).ready(loadPage);
